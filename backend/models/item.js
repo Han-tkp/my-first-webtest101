96c872706db9d4d1e5db81b7ad5fc42a01eb9a7f
Han-tkp <h4nnyza@gmail.com>
2025-09-15 22:40:03 +0700

    reall

--- backend/models/item.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Item = sequelize.define('Item', {
-    name: { type: DataTypes.STRING, allowNull: false },
-    price: { type: DataTypes.FLOAT, allowNull: false },
-    stock: { type: DataTypes.INTEGER, allowNull: false }
-  });
-  return Item;
-};
--- backend/src/models/associations.js
+++
@@ -1,7 +0,0 @@
-
-import './User.js';
-import './MachineType.js';
-import './Machine.js';
-import './Borrow.js';
-import './Return.js';
-import './Repair.js';
